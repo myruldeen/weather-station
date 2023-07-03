5fc61d67431c400e10e35804331c2849436d243f
Myrul Deen <funstudy10@gmail.com>
2023-07-03 15:22:31 +0800

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,37 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <LiquidCrystal_I2C.h>
 #include "DHT.h"
 #include <Tone32.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BMP280.h>
+#include <Adafruit_BME280.h>
 #include <ArduinoJson.h>
 #include <ArduinoJson.hpp>
-//  Anemometer demo
-//  The example demonstrates the use of anemometer.
-//  When the wind speed transmitter makes one round in one second,
-//  the transmitter outputs 20 pulses, which means that the wind speed
-//  is 1.75m/S.
-//
-const char *ssid = "Cempaka_218B@unifi";
-const char *password = "Unisel@218B";
-const char *mqtt_server = "192.168.1.109";
+#include <millisDelay.h>
 
+const char *ssid = "norazlin@unifi";
+const char *password = "bkh223811286";
+const char *mqtt_server = "test.mosquitto.org";
+const int mqtt_port = 1883;
+const char *mqtt_user = "";
+const char *mqtt_pass = "";
+
+#define SCREEN_WIDTH 128 // ORelay display width, in pixels
+#define SCREEN_HEIGHT 32 // ORelay display height, in pixels
+#define ORelay_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+
+millisDelay rebootDelay;
+unsigned long REBOOT_DELAY_MS = 24ul * 60 * 60 * 1000; // 1 day in ms
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, ORelay_RESET);
 WiFiClient espClient;
 PubSubClient client(espClient);
 unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE (50)
 char msg[MSG_BUFFER_SIZE];
-// #define WIFI_SSID "4GMIFI_1022"                     // Put your WifiSSID here
-// #define WIFI_PASS "1234567890"                      // Put your wifi password here
-// #define TOKEN "BBFF-ovHhJ8LazCmYHu8RKJhPI7ysL4UtE6" // Put your Ubidots' TOKEN
-// #define DEVICE_LABEL "ESP32"                        // MQTT client Name, please enter your own 8-12 alphanumeric character ASCII string;
-
-// #define VARIABLE_LABEL1 "Wind"        // Assing the variable label
-// #define VARIABLE_LABEL2 "Temperature" // Assing the variable label
-// #define VARIABLE_LABEL3 "Humidity"    // Assing the variable label
 
 #define DHTPIN 4
 #define DHTTYPE DHT11
@@ -42,12 +43,12 @@ char msg[MSG_BUFFER_SIZE];
 #define SEALEVELPRESSURE_HPA (1013.25)
 #define RAIN_SENSOR 19
 
-int lcdColumns = 16;
-int lcdRows = 2;
+// int lcdColumns = 16;
+// int lcdRows = 2;
 
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
+// LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 DHT dht(DHTPIN, DHTTYPE);
-Adafruit_BMP280 bmp; // I2C
+Adafruit_BME280 bme; // I2C
 
 float t, h, p, alt, f, wnd;
 unsigned int rainfall;
@@ -61,6 +62,8 @@ float speed_value = 0;
 
 int Count = 0;
 void publishData(float t_c, float t_f, float h, float p, float alt, float w, int r);
+void displayData(float t, float h, float w);
+float round2(float value);
 
 ICACHE_RAM_ATTR void onChange()
 {
@@ -115,13 +118,14 @@ void reconnect()
     {
       Serial.println("connected");
       // subscribe topic
-      client.subscribe("ESP32Weather/temperature/celcius");
-      client.subscribe("ESP32Weather/temperature/fahrenheit");
-      client.subscribe("ESP32Weather/humidity");
-      client.subscribe("ESP32Weather/pressure");
-      client.subscribe("ESP32Weather/altitude");
-      client.subscribe("ESP32Weather/wind");
-      client.subscribe("ESP32Weather/rain");
+      // client.subscribe("ESP32Weather/temperature/celcius");
+      // client.subscribe("ESP32Weather/temperature/fahrenheit");
+      // client.subscribe("ESP32Weather/humidity");
+      // client.subscribe("ESP32Weather/pressure");
+      // client.subscribe("ESP32Weather/altitude");
+      // client.subscribe("ESP32Weather/wind");
+      // client.subscribe("ESP32Weather/rain");
+      client.subscribe("ESP32Weather/weather_sensors");
     }
     else
     {
@@ -135,34 +139,52 @@ void reconnect()
 }
 void setup()
 {
+  rebootDelay.start(REBOOT_DELAY_MS);
+#if defined(ARDUINO_ARCH_ESP32)
+  enableLoopWDT(); // default appears to be 5sec
+#elif defined(ARDUINO_ARCH_ESP8266)
+  ESP.wdtEnable(5000);
+#else
+#error only ESP8266 and ESP32 reboot supported
+#endif
   pinMode(RAIN_SENSOR, INPUT);
   pinMode(BUILTIN_LED, OUTPUT);
-  lcd.init();
-  //  lcd.begin();
-  // turn on LCD backlight
-  lcd.backlight();
+  // lcd.init();
+  // //  lcd.begin();
+  // // turn on LCD backlight
+  // lcd.backlight();
   Serial.begin(115200); // Initialize serial port
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
   // subscribe topic
-  client.subscribe("ESP32Weather/temperature/celcius");
-  client.subscribe("ESP32Weather/temperature/fahrenheit");
-  client.subscribe("ESP32Weather/humidity");
-  client.subscribe("ESP32Weather/pressure");
-  client.subscribe("ESP32Weather/altitude");
-  client.subscribe("ESP32Weather/wind");
-  client.subscribe("ESP32Weather/rain");
+  // client.subscribe("ESP32Weather/temperature/celcius");
+  // client.subscribe("ESP32Weather/temperature/fahrenheit");
+  // client.subscribe("ESP32Weather/humidity");
+  // client.subscribe("ESP32Weather/pressure");
+  // client.subscribe("ESP32Weather/altitude");
+  // client.subscribe("ESP32Weather/wind");
+  // client.subscribe("ESP32Weather/rain");
+  client.subscribe("ESP32Weather/weather_sensors");
   Serial.println(F("BMP280 test"));
   unsigned status;
-  // status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
-  status = bmp.begin(0x76);
+
+  // default settings
+  // status = bme.begin();
+  // You can also pass in a Wire library object like &Wire2
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    while (1);
+  }
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+
+  status = bme.begin(0x76);
   if (!status)
   {
-    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-                     "try a different address!"));
+    Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
     Serial.print("SensorID was: 0x");
-    Serial.println(bmp.sensorID(), 16);
+    Serial.println(bme.sensorID(), 16);
     Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
     Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
     Serial.print("        ID of 0x60 represents a BME 280.\n");
@@ -170,13 +192,6 @@ void setup()
     while (1)
       delay(10);
   }
-
-  /* Default settings from datasheet. */
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 
   Serial.println();
   pinMode(BUZZER_PIN, OUTPUT);
@@ -189,6 +204,12 @@ void setup()
 
 void loop()
 {
+  if (rebootDelay.justFinished())
+  {
+    while (1)
+    {
+    } // force watch dog timer reboot
+  }
 
   //  lastDebounceTime = millis();
   //        //0.1m/s
@@ -213,23 +234,27 @@ void loop()
     //    Serial.println(" m/s");
     // set cursor to first column, first row
 
-    t = dht.readTemperature();
+    // t = dht.readTemperature();
+    // f = dht.readTemperature(true); // convert to fahrenheit
+    // h = dht.readHumidity();
+    t = bme.readTemperature();
     f = dht.readTemperature(true); // convert to fahrenheit
-    h = dht.readHumidity();
-    p = bmp.readPressure() / 100.0F;
-    alt = bmp.readAltitude(1013.25);
+    h = bme.readHumidity();
+    p = bme.readPressure() / 100.0F;
+    alt = bme.readAltitude(1013.25);
 
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.println("Wind: " + (String)speed_value + " m/s  ");
+    // lcd.clear();
+    // lcd.setCursor(0, 0);
+    // lcd.println("Wind: " + (String)speed_value + " m/s  ");
 
     delay(1000);
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("H: " + (String)h + " %");
-    lcd.setCursor(0, 1);
-    lcd.print("T: " + (String)t + " C");
+    // lcd.clear();
+    // lcd.setCursor(0, 0);
+    // lcd.print("H: " + (String)h + " %");
+    // lcd.setCursor(0, 1);
+    // lcd.print("T: " + (String)t + " C");
 
+    displayData(t, h, speed_value);
     publishData(t, f, h, p, alt, speed_value, rainfall);
 
     if (speed_value > 20.00)
@@ -251,8 +276,9 @@ void loop()
 }
 void publishData(float t_c, float t_f, float h, float p, float alt, float w, int r)
 {
-  StaticJsonDocument<32> doc;
-  char output[55];
+  DynamicJsonDocument doc(1024);
+  // StaticJsonDocument<32> doc;
+  char output[100];
   //  assign variable
   String rf;
   static char temperatureC[7];
@@ -279,24 +305,26 @@ void publishData(float t_c, float t_f, float h, float p, float alt, float w, int
     rf = "Rain";
   }
   // Publish the sensor data on their particular MQTT topics
-  client.publish("ESP32Weather/temperature/celcius", temperatureC);
-  client.publish("ESP32Weather/temperature/fahrenheit", temperatureF);
-  client.publish("ESP32Weather/humidity", String(h).c_str());
-  client.publish("ESP32Weather/pressure", pressure);
-  client.publish("ESP32Weather/altitude", altitude);
-  client.publish("ESP32Weather/wind", wind);
-  client.publish("ESP32Weather/rain", rf.c_str());
+  // client.publish("ESP32Weather/temperature/celcius", temperatureC);
+  // client.publish("ESP32Weather/temperature/fahrenheit", temperatureF);
+  // client.publish("ESP32Weather/humidity", String(h).c_str());
+  // client.publish("ESP32Weather/pressure", pressure);
+  // client.publish("ESP32Weather/altitude", altitude);
+  // client.publish("ESP32Weather/wind", wind);
+  // client.publish("ESP32Weather/rain", rf.c_str());
   // Publish json data to mqtt
-  doc["t"] = temperatureC;
-  doc["p"] = pressure;
+  doc["t"] = round2(t_c);
+  doc["p"] = round2(p);
   doc["h"] = h;
-  doc["a"] = altitude;
-  doc["w"] = wind;
-  doc["r"] = rf;
+  doc["a"] = round2(alt);
+  doc["w"] = round2(w);
+  doc["r"] = r;
 
   serializeJson(doc, output);
-  Serial.println(output);
+  // Serial.println(output);
   client.publish("ESP32Weather/weather_sensors", output);
+  // Show json data
+  serializeJson(doc, Serial);
   // Printing the Sensor Values on Serial Monitor
   Serial.println("--------------------------------------------------------");
   Serial.println("Temperature: " + (String)t_c);
@@ -306,4 +334,33 @@ void publishData(float t_c, float t_f, float h, float p, float alt, float w, int
   Serial.println("Altitude: " + (String)alt);
   Serial.println("Wind Speed: " + (String)w);
   Serial.println("--------------------------------------------------------");
+}
+
+
+float round2(float value)
+{
+  return (int)(value * 100 + 0.5) / 100.0;
+}
+
+void displayData(float t, float h, float w) {
+  display.clearDisplay();
+
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(1);
+  display.setCursor(2, 0);
+  display.print(F("T:"));
+  display.print(t, 1);
+  display.print(F(" \367C"));
+
+  display.setCursor(66, 0);
+  display.print(F("H:"));
+  display.print(h, 1);
+  display.print(F(" %"));
+
+  display.setCursor(2, 16);
+  display.print(F("Wind:"));
+  display.print(w, 1);
+
+  display.display();
+  delay(3000);
 }
